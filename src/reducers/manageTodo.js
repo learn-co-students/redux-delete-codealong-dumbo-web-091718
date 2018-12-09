262883855fd6e1a7cebf08a7a37a126cc90913be
Brian DiRito <bdirito@gmail.com>
2018-12-09 12:12:53 -0500

    weeeeeeee

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,10 +1,20 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  console.log(action.type)
   switch (action.type) {
     case 'ADD_TODO':
+      const todo = {
+        id: Math.random() * 10000000000000000,
+        text: action.payload.text
+      }
 
-      return { todos: state.todos.concat(action.payload.text) };
+      return { todos: state.todos.concat(todo) };
+
+    case "DELETE_TODO":
+      return {todos: state.todos.filter(todo => {
+        return todo.id !== action.payload
+      })}
 
     default:
       return state;
